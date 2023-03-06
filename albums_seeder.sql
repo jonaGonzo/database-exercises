65ec6c963642ad9b09d6f7cf7d9fe77bac4dacbe
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2023-03-06 09:56:51 -0600

    Updates exercise on Basic Statements

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,16 +1,7 @@
 USE codeup_test_db;
 
-DROP TABLE IF EXISTS albums;
+TRUNCATE albums;
 
-CREATE TABLE albums(
-                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                       artist VARCHAR(50) DEFAULT 'NONE',
-                       name VARCHAR(100) NOT NULL,
-                       release_date INT UNSIGNED,
-                       sales FLOAT(6,2),
-                       genre VARCHAR(50),
-                       PRIMARY KEY (id)
-);
 
 INSERT INTO albums (artist, name, release_date , genre, sales) VALUES
                                                           ('Michael Jackson', 'Thriller', 1982, 'Pop/Rock/R&B', 47.3),
