6e6d01fb4e248e80e22afeaeec7cf6a4e3a0734e
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2023-03-06 09:59:49 -0600

    Updates to updates

--- update_exercises.sql
+++ update_exercises.sql
@@ -5,16 +5,22 @@ SELECT name FROM albums;
 UPDATE albums
 SET sales = sales * 10;
 
+SELECT sales FROM albums;
+
 SELECT name FROM albums
 WHERE release_date < 1980;
 
 UPDATE albums
 SET release_date = release_date - 1000;
 
+SELECT release_date FROM albums;
+
 SELECT name FROM albums
 WHERE artist = 'Michael Jackson';
 
 UPDATE albums
 SET artist = 'Peter Jackson'
-WHERE artist = 'Michael Jackson'
+WHERE artist = 'Michael Jackson';
 
+SELECT artist FROM albums
+WHERE artist = 'Peter Jackson';
